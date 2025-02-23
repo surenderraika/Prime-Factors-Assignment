0081367e24c18282c1adfaf3c627821b522ea5f3
Surender Raika <surender.raika@student.sl.on.ca>
2025-02-22 23:18:13 -0500

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,1 +1,14 @@
+def generate_prime_factors(n):
+    if not isinstance(n, int):
+        raise ValueError("Input must be an integer")
+    if n == 1:
+        return []
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
 
