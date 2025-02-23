19d16b99fedfa323e64948a6d36d6ee692184f46
Surender Raika <surender.raika@student.sl.on.ca>
2025-02-22 23:53:33 -0500

    Update test_prime.py

--- test_prime.py
+++ test_prime.py
@@ -7,7 +7,7 @@ def test_invalid_input():
         generate_prime_factors("hello")
     with pytest.raises(ValueError):
         generate_prime_factors(4.5)
-        def test_prime_factors_of_1():
+    def test_prime_factors_of_1():
     """Test that when input is 1, an empty list is returned"""
     assert generate_prime_factors(1) == []
     def test_prime_factors_of_2():
