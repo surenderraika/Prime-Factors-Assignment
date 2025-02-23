968a578850a5d8d4eca9be846db5f19dedb5afc5
Surender Raika <surender.raika@student.sl.on.ca>
2025-02-22 23:45:28 -0500

    "Step 2: Handle input 1"

--- test_prime.py
+++ test_prime.py
@@ -7,4 +7,8 @@ def test_invalid_input():
         generate_prime_factors("hello")
     with pytest.raises(ValueError):
         generate_prime_factors(4.5)
+        def test_prime_factors_of_1():
+    """Test that when input is 1, an empty list is returned"""
+    assert generate_prime_factors(1) == []
+        
 
