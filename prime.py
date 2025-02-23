33900c67b865c53bd6dbf612e39f6d25a2d40435
Surender Raika <surender.raika@student.sl.on.ca>
2025-02-22 23:24:17 -0500

    Fixed Pylint warnings: added docstrings & removed trailing newline

--- prime.py
+++ prime.py
@@ -1,8 +1,15 @@
+"""
+Prime Factors Module
+This module contains a function to generate prime factors of a given number.
+"""
+
 def generate_prime_factors(n):
+    """Returns a list of prime factors for the given number n."""
     if not isinstance(n, int):
         raise ValueError("Input must be an integer")
     if n == 1:
         return []
+    
     factors = []
     divisor = 2
     while n > 1:
@@ -11,4 +18,3 @@ def generate_prime_factors(n):
             n //= divisor
         divisor += 1
     return factors
-
